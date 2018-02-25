756d63a288d1c4733fdf252a0bc50ed503afe0af
seung7 <seungmin.lee777@gmail.com>
2018-02-25 13:08:25 -0800

    Latest Mega code. It skips some counts from the decoder

--- ArduinoMega/MegaDecoTimer/MegaDecoTimer.ino
+++ ArduinoMega/MegaDecoTimer/MegaDecoTimer.ino
@@ -11,11 +11,11 @@
 #define D5 A5
 #define D6 A6
 #define D7 A7
-#define COOLCLOCKOUT 11
 //#define READING_DX 11
 
-#define CLOCKOUT 9
-#define RESET 53
+#define Decoder_ClockOut 8
+#define Timer_ClockOut 11
+#define RESET 52
 
 byte d0_store = 1;
 byte d1_store = 0;
@@ -34,6 +34,7 @@ byte prev_result1 = 0;
 /* Timer */
 long the_time_now = 0;
 long the_count_now = 0;
+int toggle0 = 0;
 
 /* Motors */
 
@@ -52,18 +53,18 @@ int M1_DIR = 0;
 void read_and_store_decoder_data() {
  result0 = PINF & B11111111;
  
- if ( prev_result0 != result0 ){
-  the_count_now++;
+ //if ( prev_result0 != result0 ){
+ // the_count_now++;
   prev_result0 = result0;
   //Serial.print( result0 , BIN);          // Motor 0 Angular Position
   //Serial.print( " " );
   Serial.print( the_time_now );
   Serial.print( " " );
-  Serial.print( the_count_now );
+  //Serial.print( the_count_now );
   Serial.print( " " );
   Serial.print( result0 );          // Motor 0 Angular Position
   Serial.println();
- }
+// }
 }
  
 
@@ -92,33 +93,65 @@ void reset_decoders() {
  digitalWriteFast(RESET, HIGH);
 }
 
-void setup_8_MHz_clock() {
- pinModeFast (CLOCKOUT, OUTPUT); 
- // set up Timer 1
- TCCR1A = bit (COM1A0);  // toggle OC1A on Compare Match
- TCCR1B = bit (WGM12) | bit (CS10);   // CTC, no prescaling
- OCR1A =  0;       // output every cycle
+
+/** This timer actually creates 2MHz freuqncy, but it works as decoder clock with 1MHz.(Thus, this routine is called 1Mhz_decoder_clock).
+ *  First 2MHz, the signal will goes up, and next frequency, the signal will goes down. As a result it creates 1Mhz Clock output for the decoder.    
+ *  Look at the routine ISR(TIMER0_COMPA_vect) to check the details..
+ */
+void setup_1MHz_decoder_clock() {
+  // Decoder_ClockOut(Pin#8 ) as Output
+  pinModeFast (Decoder_ClockOut, OUTPUT); 
+  //set timer0 interrupt at 1MHz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  OCR0A = 7;// = (16*10^6) / (2*10^6*1) - 1 (must be <256);//set compare match register for 2MHz increments
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS10 bits for 1 prescaler (= no prescalar)
+  TCCR0B |= (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
 }
 
-void setup_cool_timer() {
-//set timer1 interrupt at 1z
-  cli();//stop interrupts
+/** This timer will shows the time everytime, when the slot detectors detects the change 
+ * When 5V applies, The motor provides 2000RPM which is converts to 33.333Hz.
+ * 1 full rotation has 400 slots. so it gives 33.333*400 = 13,333Hz
+ * Thus the timer should be bigger than 13,333Hz. 50KHz is set for that reason.
+ */
+void setup_50KHz_timer() {
+//set timer1 interrupt at 50KHz
  TCCR1A = 0;// set entire TCCR1A register to 0
  TCCR1A |= (1 << COM1A0);  // toggle OC1A on Compare Matc
- pinModeFast (COOLCLOCKOUT, OUTPUT); 
+ pinModeFast (Timer_ClockOut, OUTPUT); // pin#11 is used for the Timer_ClockOut
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
-  // set compare match register for 1hz increments. 16MHz/ (Prescalr*desire interrupted frequency)-1
-  OCR1A = 0.5625;// = (16*10^6) / (10000*1024) - 1 (must be <65536)
+  // set compare match register for 50Khz increments. 16MHz/ (Prescalr*desire interrupted frequency)-1
+  OCR1A = 1000;// = (16*10^6) / (50,000*1) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS12 and CS10 bits for 1024 prescaler 
-  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  TCCR1B |= (1 << CS10);  
   // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
-
-  sei();//allow interrupts
+  TIMSK1 |= (1 << OCIE1A); 
 }
+
+//Setting decoder clock to be 1Mhz
+ISR(TIMER0_COMPA_vect){
+  if (toggle0){
+    digitalWrite(Decoder_ClockOut,HIGH);
+    toggle0 = 0;
+  }
+  else{
+    digitalWrite(Decoder_ClockOut,LOW);
+    toggle0 = 1;
+  }
+}
+
+//Everytime the time passes 1/50,000s, it increase the variable 'the_time_now'
+ISR(TIMER1_COMPA_vect){
+ the_time_now++;
+ }
 
 void setup_motors() {
  pinModeFast (M0_EN,OUTPUT);
@@ -140,24 +173,24 @@ void M0_change_dir() {
  digitalWrite(M0_DIR2, (!M0_DIR) ? HIGH : LOW);
 }
 
-ISR(TIMER1_COMPA_vect){
- the_time_now++;
- }
-
-
-
 
  /**************** MAIN ****************/
 
 void setup() {
- // setup_8_MHz_clock();         // for clocking decoders; too lazy to get external one for them
- setup_cool_timer();
+ //stop interrupts
+ cli();
+
+//This will be clock frequency (Sampling Frequecy) for decoder. The expected frequency for the encoder is ~13kHz. 1MHz >> 13KHz, and the decoder should collect the data correctly.
+ setup_1MHz_decoder_clock(); 
+ setup_50KHz_timer();
  setup_decoder_8bit_input();
  reset_decoders();
 
  setup_motors();
  M0_start();
  
+ //allow interrupts
+ sei();
  Serial.begin (250000);
  Serial.println("hi");
 }
@@ -171,4 +204,5 @@ void loop() {
  read_and_store_decoder_data();
  result1 = D0.d0 + (D0.d1 << 1) + (D0.d2 << 2) + (D0.d3 << 3) + (D0.d4 << 4) + (D0.d5 << 5) + (D0.d6 << 6) + (D0.d7 << 7);
  */
+ //Serial.print("test");
 }
